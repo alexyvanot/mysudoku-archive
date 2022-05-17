f6a9976125e5ced1a23c848e06969d6673b156a8
Alexy <48055002+alexyvanot@users.noreply.github.com>
2022-05-17 10:39:14 +0200

    Rework of MainApp.java + Added app icon in resources...

--- src/main/java/com/intech/mysudoku/application/MySudokuApp.java
+++ src/main/java/com/intech/mysudoku/application/MySudokuApp.java
@@ -10,13 +10,47 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class MySudokuApp extends Application {
 
+	private Stage primaryStage;
+	private Parent root;
+	
+	@Override
+	public void init() throws Exception {
+		super.init();
+	}
+	
     @Override
     public void start(Stage primaryStage) throws Exception {
-    	
+    	this.primaryStage = primaryStage;
+    	initRootLayout();
+        
+    }
+    
+    public void initRootLayout() {
+        try {
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getClassLoader().getResource("views/titleScreen.fxml"));
+            Parent root = loader.load();
+            TitleScreenController titleScreenController = loader.getController();
+            Scene scene = new Scene(root);
+            titleScreenController.setStartScene(scene);
+            this.primaryStage.setTitle("MySudoku");
+            primaryStage.getIcons().add(new Image("file:resources/images/icon.png"));
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
+    
+    public void invocateSudoku() {
     	/**
     	 * Sudoku invocator
     	 */
@@ -29,25 +63,16 @@ public class MySudokuApp extends Application {
 		solveThisSudoku.setBoard(sudokuBoard).solve(sudokuBoard.getCells(), 1);
 		System.out.println();
 		System.out.println(sudokuBoard);
-    	
-    	/* "Deprecated"
-    	int N = 9, K = 20;
-		Sudoku sudoku = new Sudoku(N, K);
-		sudoku.fillValues();
-		sudoku.printSudoku(); // Show in console 
-		*/
 		
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("views/titleScreen.fxml"));
-            Parent root = loader.load();
-            TitleScreenController titleScreenController = loader.getController();
-            Scene scene = new Scene(root);
-            titleScreenController.setStartScene(scene);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    }
+    
+    @Override
+    public void stop() throws Exception {
+    	super.stop();
+    }
+    
+    public static void main(String[] args) {
+    	Application.launch(args);
     }
 
 
